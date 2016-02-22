af4481da3d6120677a38181a4c90c068de3a2c40
Jorge <jbernal0019@yahoo.es>
2016-02-22 14:15:50 -0500

    Updated stylesheet paths to bower_components for consistency

--- demo/config.js
+++ demo/config.js
@@ -4,7 +4,7 @@ require.config({
 
     // third party components
     jquery: ['https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min', 'jquery/dist/jquery.min'],
-    jquery_ui: ['https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min', 'query-ui/jquery-ui.min'],
+    jquery_ui: ['https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min', 'jquery-ui/jquery-ui.min'],
   },
 
   // use packages to be able to use relative paths
